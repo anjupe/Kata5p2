05120a81e2f2df239eae0c682387736cf41c27f7
anjupe <anjupe@gmail.com>
2017-12-05 11:22:03 +0000

    Kata 5 parte 2. Versión final

--- src/kata5p2/MAIN/Kata5p2.java
+++ src/kata5p2/MAIN/Kata5p2.java
@@ -1,31 +1,32 @@
 package kata5p2.MAIN;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import kata5p2.model.Histogram;
+import kata5p2.model.Mail;
 import kata5p2.view.HistogramDisplay;
 import kata5p2.view.MailHistogramBuilder;
-import kata5p2.view.MailListReader;
+import kata5p2.view.MailListReaderDDBB;
 
 public class Kata5p2 {
     
     private List<String> mailList;
     private Histogram<String> histogram;
     
-    public static void main(String[] args) throws IOException {     
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {     
         Kata5p2 kata = new Kata5p2();
         kata.execute(); 
     }
     
-    private void execute() throws IOException{
+    private void execute() throws IOException, SQLException, ClassNotFoundException{
         input();
         process();
         output();
     }
     
-    private void input() throws IOException{
-        String filename = "C:\\Users\\Experimentos\\Documents\\NetBeansProjects\\Kata4\\emailsfile.txt";
-        mailList = MailListReader.read(filename);
+    private void input() throws IOException, ClassNotFoundException, SQLException{
+        mailList = MailListReaderDDBB.read();
     }
     
     private void process(){
--- src/kata5p2/view/MailListReaderDDBB.java
+++ src/kata5p2/view/MailListReaderDDBB.java
@@ -1,24 +1,28 @@
 package kata5p2.view;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Statement;
 
 public class MailListReaderDDBB {
-    public static List<String> read (String filename) throws FileNotFoundException, IOException{
+    public static List<String> read () throws IOException, ClassNotFoundException, SQLException{
         List<String> mailList = new ArrayList();
-        BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
-        String mail;
-        while ((mail = reader.readLine()) != null){
-            if (mail.contains("@")){
-                mailList.add(mail);
-            }
+        
+        Class.forName("org.sqlite.JDBC");
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:KATA.sDB");
+        
+        Statement statement = connection.createStatement();
+        
+        ResultSet rs = statement.executeQuery("SELECT * FROM mail;");
+        while (rs.next()){
+            mailList.add(rs.getString("mail"));
         }
-        reader.close();
+        
         return mailList;
     }
 
